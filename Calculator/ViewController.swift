df06a6dd5d4164fb8a111d726981bd43211e231e
Yanni Speron <yannisperon@gmail.com>
2020-09-14 02:57:33 -0500

    Added everything

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,116 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var finalAmountLabel: UILabel!
+    @IBOutlet weak var customTipTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        billAmountTextField.placeholder = Locale.current.currencySymbol
+        
+        customTipTextField.isHidden = false
+        customTipTextField.alpha = 0.0
+        
+        let fmt = NumberFormatter()
+        fmt.allowsFloats = true
+        fmt.minimumFractionDigits = 2
+        fmt.maximumFractionDigits = 2
+        fmt.minimumIntegerDigits = 1
+        fmt.roundingMode = .up
+        fmt.numberStyle = .currency
+        
+        if ((NSDate().timeIntervalSince1970 - UserDefaults.standard.double(forKey: "time")) <= 600) {
+            print("Loading values")
+            let billAmount = UserDefaults.standard.double(forKey: "billAmount")
+            let finalAmount = UserDefaults.standard.double(forKey: "finalAmount")
+            let tipAmount = UserDefaults.standard.double(forKey: "tipAmount")
+            let selectedIndex = UserDefaults.standard.integer(forKey: "selectedTipAmount")
+            let customTipValue = UserDefaults.standard.double(forKey: "customTipValue")
+            
+            billAmountTextField.text = String(billAmount).replacingOccurrences(of: ".", with: String(Locale.current.decimalSeparator ?? "."))
+            tipAmountLabel.text = fmt.string(from: NSNumber(value: tipAmount)) ?? fmt.string(from: 0.0)
+            
+            finalAmountLabel.text = fmt.string(from: NSNumber(value: finalAmount)) ?? fmt.string(from: 0.0)
+            
+            customTipTextField.text = String(customTipValue)
+            tipAmountSegmentedControl.selectedSegmentIndex = selectedIndex
+            
+            if (selectedIndex == 3) {
+                customTipTextField.alpha = 1.0
+            }
+        } else {
+            tipAmountLabel.text = fmt.string(from: 0.0)
+            finalAmountLabel.text = fmt.string(from: 0.0)
+        }
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func customTipAmountChanged(_ sender: Any) {
+        calculateTip()
+    }
+    
+    @IBAction func billAmountFieldChanged(_ sender: Any) {
+        calculateTip()
+    }
+    
+    @IBAction func tipAmountControlChanged(_ sender: Any) {
+        if (tipAmountSegmentedControl.selectedSegmentIndex == 3) {
+            UIView.animate(withDuration: 0.25) {
+                self.customTipTextField.alpha = 1.0
+            }
+            customTipTextField.becomeFirstResponder()
+        } else {
+            UIView.animate(withDuration: 0.25) {
+                self.customTipTextField.alpha = 0.0
+            }
+            view.endEditing(true)
+        }
+        calculateTip()
+    }
+    
+    private func calculateTip() {
+        var billAmount = 0.0
+        if let unwrappedValue = billAmountTextField.text {
+            billAmount = Double(unwrappedValue.replacingOccurrences(of: String(Locale.current.decimalSeparator ?? "."), with: ".")) ?? 0.0
+        }
+        
+        var customTipValue = 0.0
+        if let unwrappedValue = customTipTextField.text {
+            customTipValue = Double(unwrappedValue.replacingOccurrences(of: String(Locale.current.decimalSeparator ?? "."), with: ".")) ?? 0.0
+        }
+        
+        let tipValues = [0.15, 0.2, 0.25, customTipValue / 100.0]
+        
+        let tipPercentage = tipValues[tipAmountSegmentedControl.selectedSegmentIndex]
+        
+        let fmt = NumberFormatter()
+        fmt.allowsFloats = true
+        fmt.minimumFractionDigits = 2
+        fmt.maximumFractionDigits = 2
+        fmt.minimumIntegerDigits = 1
+        fmt.roundingMode = .up
+        fmt.numberStyle = .currency
+        
+        let tipAmount = tipPercentage * billAmount
+        tipAmountLabel.text = fmt.string(from: NSNumber(value: tipAmount)) ?? fmt.string(from: 0.0)
+        
+        let finalAmount = tipAmount + billAmount
+        finalAmountLabel.text = fmt.string(from: NSNumber(value: finalAmount)) ?? fmt.string(from: 0.0)
+        
+        UserDefaults.standard.set(billAmount, forKey: "billAmount")
+        UserDefaults.standard.set(finalAmount, forKey: "finalAmount")
+        UserDefaults.standard.set(tipAmount, forKey: "tipAmount")
+        UserDefaults.standard.set(tipAmountSegmentedControl.selectedSegmentIndex, forKey: "selectedTipAmount")
+        UserDefaults.standard.set(customTipValue, forKey: "customTipValue")
+        
+        UserDefaults.standard.set(NSDate().timeIntervalSince1970, forKey: "time")
+    }
 }
 
